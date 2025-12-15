32e1a37b68e905fdd2730a64839425829d60e0e2
Similien Michel Fils <msimilien3@hotmail.com>
2025-12-14 22:59:29 -0500

    Add render api link

--- src/Snsc/Program.cs
+++ src/Snsc/Program.cs
@@ -1,7 +1,10 @@
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Microsoft.Extensions.DependencyInjection;
 using MudBlazor.Services;
 using Snsc;
+using System;
+using System.Net.Http;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
@@ -10,7 +13,8 @@ builder.RootComponents.Add<HeadOutlet>("head::after");
 //builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 builder.Services.AddScoped(sp => new HttpClient
 {
-    BaseAddress = new Uri("https://localhost:7092/")
+    //BaseAddress = new Uri("https://localhost:7092/")
+      BaseAddress = new Uri("https://snsc-api.onrender.com/")
 });
 builder.Services.AddMudServices();
 await builder.Build().RunAsync();
